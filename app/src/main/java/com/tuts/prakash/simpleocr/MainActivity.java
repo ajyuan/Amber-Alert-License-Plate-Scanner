75fb3227a45e310293d063af15db2347cc1efd7a
Austin Yuan <austinyuan2@gmail.com>
2020-01-18 18:24:28 -0800

    dep upgrades

--- app/src/main/java/com/tuts/prakash/simpleocr/MainActivity.java
+++ app/src/main/java/com/tuts/prakash/simpleocr/MainActivity.java
@@ -2,15 +2,17 @@ package com.tuts.prakash.simpleocr;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
+
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.text.TextBlock;
@@ -132,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
                                     stringBuilder.append(item.getValue());
                                     stringBuilder.append("\n");
                                 }
+                                //Log.d("SCAN", stringBuilder.toString());
                                 mTextView.setText(stringBuilder.toString());
                             }
                         });
